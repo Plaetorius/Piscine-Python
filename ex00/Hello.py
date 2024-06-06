4e3ae373bd176d77a8a39d7100a6429ed644c203
Tag6362 <tag6362@gmail.com>
2024-06-06 13:28:08 +0200

    ex00

--- ex00/Hello.py
+++ ex00/Hello.py
@@ -4,32 +4,60 @@ ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
 
 
-# List
+# ======================= LIST =======================
 
-ft_list[1] = "World!"
+# Direct index
+# ft_list[1] = "World!"
 
-ft_list[-1] = "World!"
+# Reverse index
+# ft_list[-1] = "World!"
 
-ft_list.pop(-1)
-ft_list.append("World!")
+# Pop n' append
+# ft_list.pop(-1)
+# ft_list.append("World!")
 
-ft_list = [word if word != "tata!" else "World!" for word in ft_list]
+# List comprehension
+# ft_list = [word if word != "tata!" else "World!" for word in ft_list]
 
-def change_list(word):
-	return (word if word != "tata!" else "World!")
+# Map with function
+# def change_list(word):
+# 	return (word if word != "tata!" else "World!")
 
-ft_list = list(map(change_list, ft_list))
+# ft_list = list(map(change_list, ft_list))
 
-ft_list = list(map(lambda word : (word if word != "tata!" else "World!"), ft_list))
+# Map with lambda
+# ft_list = list(map(lambda word : (word if word != "tata!" else "World!"), ft_list))
 
-ft_list = ' '.join(ft_list).replace("tata!", "World!")
+# Conversion to string
+ft_list = ' '.join(ft_list).replace("tata!", "World!").split(' ')
 
+# ======================= TUPLE =======================
 
-# Tuple
+# Tuples are unchangeable, thus, conversion to list and then back into a tuple
+ft_tuple = tuple(list(map(lambda word : (word if word != "toto!" else "France!"), ft_tuple)))
 
-# Set
+# ======================= SET =======================
 
-# Dictionary
+# Sets are unordered, thus popping pops a random value
+# (printing also shows them in a random order)
+# Remove n' Add
+ft_set.remove("tutu!")
+ft_set.add("Paris!")
+
+# ======================= DICT =======================
+
+# Change via key
+ft_dict['Hello'] = "42Paris!"
+
+# Simple change
+
+# Pop
+# ft_dict.pop('Hello')
+# ft_dict['Hello'] = "42Paris!"
+
+# Popitem (pops last element, returns pair <key, value>)
+# ft_dict.popitem()
+# ft_dict['Hello'] = '42Paris!'
 
 
 print(ft_list)
