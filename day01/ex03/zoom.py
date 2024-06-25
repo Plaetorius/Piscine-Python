16ef2b1449e67d8b55e23364d4eb036c2f827175
Tom GERNEZ <tgernez@student.42.fr>
2024-06-25 18:02:32 +0200

    working on zoom, added plt

--- day01/ex03/zoom.py
+++ day01/ex03/zoom.py
@@ -1,10 +1,29 @@
 import numpy as np
 from load_image import ft_load
+from PIL import Image
+import matplotlib.pyplot as plt
+import sys
 
 
 def ft_zoom(image: np.array) -> np.array:
     try:
-        image = Image.fromarray(image[200:200], 'RGB')
-        image.save()
+        height, width = image.shape[0], image.shape[1]
+        # TODO zoom me
+        zoomed_image = Image.fromarray(image, 'RGB')
+        zoomed_image.save("zoomed.jpg") # TODO maybe remove
+        plt.imshow(zoomed_image)
+        plt.show()
+        return image
+    except Exception as e:
+        print(f"Error: {e}")
+        return np.array([])
+
+if __name__ == "__main__":
+    try:
+        if (len(sys.argv) == 2):
+            array = ft_zoom(ft_load(sys.argv[1]))
+            print(f"The shape of the zoomed image is: {array.shape}")
+        else:
+            print("Please provide an argument")
     except Exception as e:
         print(f"Error: {e}")
