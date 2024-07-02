34fdbca8d596a0377ccfb4fb8935d5ee1756b165
Tag6362 <tag6362@gmail.com>
2024-07-02 23:46:39 +0200

    level00

---
+++ day02/ex01/load_csv.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import sys
+
+
+def load(path: str) -> pd.DataFrame:
+    """
+    Load an CSV file in to a pandas' DataFrame
+
+    Parameters:
+    path (str): the path to the image.
+
+    Returns:
+    pd.DataFrame with the dimensions of the CSV file and its content
+
+    Raises:
+    FileNotFoundError: If the path doesn't lead to any image
+    Exception: other errors (corrupted / wrong file...)
+    """
+    try:
+        return pd.read_csv(path)
+    except FileNotFoundError:
+        print("File not found!")
+        return pd.DataFrame([])
+    except Exception:
+        print("Unsupported file!")
+        return pd.DataFrame([])
+
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) == 2:
+            file = load(sys.argv[1])
+            print(f"Loading dataset of dimensions {file.shape}")
+            print(file)
+    except Exception as e:
+        print(f"Error: {e}")
